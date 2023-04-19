bf23725fe0936ef2d3fa3c1a7a101ea4f61500fd
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-19 03:43:45 +0000

    deploy: 5144e11fe1a2d50b77fd742ac8be895c43877858

--- code.js
+++ code.js
@@ -10,8 +10,23 @@ let todos = [
 let renderTodoApp = function() {
   // Follow the instructions on my.kenzie.academy to complete the assignment.
   // Your Code Here
+  let theApp = document.getElementById("app");
+  let h3Element1 = document.createElement("h3");
+  let ulElement = document.createElement("ul");
 
- 
+  h3Element1.innerText = "TodoList";
+  theApp.append(h3Element1);
+
+  for (let index = 0; index < todos.length; index +=1) {
+    let liElement = document.createElement("li");
+    let currentTask = todos[index];
+
+    liElement.innerText = currentTask;
+    liElement.className = "list-item";
+    ulElement.append(liElement);
+  }
+
+  theApp.append(ulElement);
 }
 
 renderTodoApp();
